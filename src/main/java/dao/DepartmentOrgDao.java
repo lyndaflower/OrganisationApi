ce0c448871f233af9fafe2915fa5b06f56e4fcca
lyndaflower <umurerwalynda@gmail.com>
2019-09-30 03:19:37 +0200

    add class new

--- src/main/java/dao/DepartmentOrgDao.java
+++ src/main/java/dao/DepartmentOrgDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.DepartmentOrg;
+
 public interface DepartmentOrgDao {
+
+    //create
+    void add(DepartmentOrg departmentOrg);
+    void addDepartmentOrgToNews(DepartmentOrg department, News news);
+
+    //read
+    //List<DepartmentOrg> getAll();
+    //  DepartmentOrg findById(int id);
+    //    List<News> getAllNewsByDepartmentOrg(int departmentOrgId);
+
+    //update
+    void update(int id, String name, String description, int numberOfEmployees);
+
+    //delete
+    void deleteById(int id);
+    void clearAll()
 }
