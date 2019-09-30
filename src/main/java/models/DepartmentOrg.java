6edf3e3caf0f4f1168f2e715fb99642939482af8
lyndaflower <umurerwalynda@gmail.com>
2019-09-30 02:36:52 +0200

    add gitignore and dependencies

--- src/main/java/models/DepartmentOrg.java
+++ src/main/java/models/DepartmentOrg.java
@@ -1,4 +1,17 @@
 package models;
 
 public class DepartmentOrg {
+
+//    private String name;
+//    private String description;
+//    private int numberOfEmployees;
+//    private  int id;
+//
+//    public DepartmentOrg(String name, String description, int numberOfEmployees){
+//        this.name = name;
+//        this.description= description;
+//        this.numberOfEmployees= numberOfEmployees;
+
+
+    }
 }
