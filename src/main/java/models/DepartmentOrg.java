9964a38ff2ebfcc9d556d419573ac84be7aaf608
lyndaflower <umurerwalynda@gmail.com>
2019-09-30 03:02:39 +0200

    add sql

--- src/main/java/models/DepartmentOrg.java
+++ src/main/java/models/DepartmentOrg.java
@@ -2,15 +2,40 @@ package models;
 
 public class DepartmentOrg {
 
-//    private String name;
-//    private String description;
-//    private int numberOfEmployees;
-//    private  int id;
-//
-//    public DepartmentOrg(String name, String description, int numberOfEmployees){
-//        this.name = name;
-//        this.description= description;
-//        this.numberOfEmployees= numberOfEmployees;
 
+    private String name;
+    private String description;
+    private int numEmployees;
+    private  int id;
 
+    public DepartmentOrg(String name, String description, int numEmployees) {
+        this.name = name;
+        this.description = description;
+        this.numEmployees = numEmployees;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String finance) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String handle_finance_business) {
+        this.description = description;
+    }
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int i) {
+        this.numEmployees = numEmployees;
+    }
 }
--- src/test/java/models/DepartmentOrgTest.java
+++ src/test/java/models/DepartmentOrgTest.java
@@ -1,6 +1,8 @@
 package models;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -10,4 +12,47 @@ public class DepartmentOrgTest {
     public void setUp() throws Exception {
     }
 
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getNameReturnsCorrectName() throws Exception {
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();
+        assertEquals("finance", testDepartmentOrg.getName());
+    }
+
+    @Test
+    public void setNameSetsCorrectName() throws Exception {
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();
+        testDepartmentOrg.setName("finance");
+        assertNotEquals("accounting", testDepartmentOrg.getName());
+    }
+
+
+    @Test
+    public void getDescriptionReturnsCorrectDescription() throws Exception{
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();
+        assertEquals("handle finance business", testDepartmentOrg.getDescription());
+    }
+
+    @Test
+    public void setDescriptionSetsCorrectDescription() throws Exception {
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();
+        testDepartmentOrg.setDescription("handle finance business");
+        assertNotEquals("handle accounting business", testDepartmentOrg.getDescription());
+    }
+
+    @Test
+    public void getNumberOfEmployeesReturnsCorrectNumEmployee() {
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();epartment();
+        assertEquals(12, testDepartmentOrg.getNumEmployees());
+    }
+
+    @Test
+    public void setNumberOfEmployeesSetsCorrectNumber() {
+        DepartmentOrg testDepartmentOrg= setupDepartmentOrg();
+        testDepartmentOrg.setNumEmployees(12);
+        assertNotEquals(13, testDepartmentOrg.getNumEmployees());
+    }
 }
